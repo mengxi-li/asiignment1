7d207da82182876ff93074025992a5f1881c0b16
mengxi li <mengxi616@gmail.com>
2020-10-03 17:49:10 -0400

    second

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = parseInt(process.env.PORT || "3000")
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/aboutme', express.static(path.join(__dirname, 'public/aboutme.html')))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
