fd8f0af7cba50b10f5524ddc34af7a7dfca25b77
mengxi li <mengxi616@gmail.com>
2020-10-06 00:03:31 -0400

    7

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const port = parseInt(process.env.PORT || "3000")
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use('/aboutme', express.static(path.join(__dirname, 'public/aboutme.html')))
-app.use('/contactme', express.static(path.join(__dirname, 'public/contactme.html')))
-app.use('/project', express.static(path.join(__dirname, 'public/project.html')))
-app.use('/services', express.static(path.join(__dirname, 'public/services.html')))
+app.set('view engine', 'ejs');
+app.use( express.static( "public" ) );
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/aboutme', function(req, res) {
+  res.render('aboutme');
+});
+app.get('/contactme', function(req, res) {
+  res.render('contactme');
+});
+app.get('/project', function(req, res) {
+  res.render('project');
+});
+app.get('/services', function(req, res) {
+  res.render('services');
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
